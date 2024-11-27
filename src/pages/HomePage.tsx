f1410e9c128472c3238fa2fc9038398f662aef06
Rashmi Ranjan Sahoo <sahoorashmiranjan11@gmail.com>
2024-11-27 22:50:38 +0530

    lint issue resolved for pipline

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,16 +1,8 @@
-import React, { useState, useEffect } from "react";
+import { useEffect } from "react";
 import AOS from "aos";
 import "aos/dist/aos.css";
-import Navbar from "../components/Navbar";
 import Hero from "../components/Hero";
 import FeaturedStocks from "../components/FeaturedStocks";
-import SearchBar from "../components/SearchBar";
-import Footer from "../components/Footer";
-import { getStockTimeSeries } from "../services/remoteApi";
-import { StockData } from "../types";
-import StockCardDetail from "../components/StockCardDetail";
-import SkeletonCard from "../components/SkeletonCard";
-import ReactPaginate from "react-paginate";
 
 const HomePage = () => {
   useEffect(() => {
--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -1,11 +1,7 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import AOS from "aos";
 import "aos/dist/aos.css";
-import Navbar from "../components/Navbar";
-import Hero from "../components/Hero";
-import FeaturedStocks from "../components/FeaturedStocks";
 import SearchBar from "../components/SearchBar";
-import Footer from "../components/Footer";
 import { getStockTimeSeries } from "../services/remoteApi";
 import { StockData } from "../types";
 import StockCardDetail from "../components/StockCardDetail";
